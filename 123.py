e159eebeb5115936c3595f11667693deefd0b209
Maciej Dziobek <maciejdziob@gmail.com>
2020-07-09 13:18:14 +0200

    Inital Commit: Basic structure and simple shape classes. Tests don't pass.

---
+++ 123.py
@@ -0,0 +1,47 @@
+
+print("podaj 3 liczby")
+a=int(input())
+b=int(input())
+c=int(input())
+
+if (a>b):
+    naj=a
+else:
+    naj=b
+if(c>naj):
+    naj=c
+print("nawieksza liczba to", naj)
+
+
+if(a>b):
+    jeden=a
+else:
+    jeden=b
+if(c>jeden):
+    jeden=c
+
+if(jeden != a and jeden != b):
+    if(a>b):
+        dwa=a
+    else:
+        dwa=b
+if(jeden != a and jeden != c):
+    if(a>c):
+        dwa=a
+    else:
+        dwa=c
+if(jeden != b and jeden != c):
+    if(c>b):
+        dwa=c
+    else:
+        dwa=b
+
+if(jeden !=c and dwa !=c):
+    trzy=c
+if(jeden !=b and dwa !=b):
+    trzy=b
+if(jeden !=a and dwa !=a):
+    trzy=a
+
+
+print(jeden, dwa, trzy)
